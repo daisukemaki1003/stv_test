4fb609f9cecb01ee3def89e639becbafdd81a34e
牧大佑 <daisukemaki1003@gmail.com>
2022-10-29 23:07:05 +0900

    [add] カレンダー

--- lib/view/calendar.dart
+++ lib/view/calendar.dart
@@ -16,6 +16,7 @@ class CalendarPage extends StatelessWidget {
     const weekHeaderColor = Color.fromARGB(255, 237, 237, 237);
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(title: const Text("カレンダー")),
       body: Column(
         children: [
@@ -23,10 +24,10 @@ class CalendarPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               /// 今日ボタン
-              _todayButton(),
+              todayButton(),
 
               /// 月選択ピッカー
-              _monthPicker(),
+              monthPicker(),
 
               Container(),
             ],
@@ -39,37 +40,112 @@ class CalendarPage extends StatelessWidget {
             saturdayWeekHeaderTextStyle,
             sundayWeekHeaderTextStyle,
           ),
+
+          weekRow(),
+          weekRow(),
+          weekRow(),
+          weekRow(),
+          weekRow(),
+          weekRow(),
         ],
       ),
     );
   }
 
-  Container weekHeader(
+  Widget weekRow() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _dateCell(false, false, false, false),
+          _dateCell(false, false, false, false),
+          _dateCell(false, false, false, false),
+          _dateCell(false, false, false, false),
+          _dateCell(false, false, false, false),
+          _dateCell(false, false, true, false),
+          _dateCell(true, true, false, true),
+        ],
+      ),
+    );
+  }
+
+  Widget _dateCell(bool pran, bool today, bool saturday, bool sunday) {
+    final textColor;
+    if (saturday) {
+      textColor = Colors.blue;
+    } else if (sunday) {
+      textColor = Colors.red;
+    } else {
+      textColor = Colors.black;
+    }
+
+    return Ink(
+      height: 65,
+      width: 40,
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          Container(
+            decoration: today
+                ? const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  )
+                : null,
+            child: Center(
+              child: Text(
+                "12",
+                style: TextStyle(
+                  color: today ? Colors.white : textColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+
+          /// 予定が存在する
+          if (pran)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: 10,
+                height: 10,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+
+  // padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+  Widget weekHeader(
       Color weekHeaderColor,
       TextStyle defaultWeekHeaderTextStyle,
       TextStyle saturdayWeekHeaderTextStyle,
       TextStyle sundayWeekHeaderTextStyle) {
     return Container(
       color: weekHeaderColor,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("月", style: defaultWeekHeaderTextStyle),
-            Text("火", style: defaultWeekHeaderTextStyle),
-            Text("水", style: defaultWeekHeaderTextStyle),
-            Text("木", style: defaultWeekHeaderTextStyle),
-            Text("金", style: defaultWeekHeaderTextStyle),
-            Text("土", style: saturdayWeekHeaderTextStyle),
-            Text("日", style: sundayWeekHeaderTextStyle),
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text("月", style: defaultWeekHeaderTextStyle),
+          Text("火", style: defaultWeekHeaderTextStyle),
+          Text("水", style: defaultWeekHeaderTextStyle),
+          Text("木", style: defaultWeekHeaderTextStyle),
+          Text("金", style: defaultWeekHeaderTextStyle),
+          Text("土", style: saturdayWeekHeaderTextStyle),
+          Text("日", style: sundayWeekHeaderTextStyle),
+        ],
       ),
     );
   }
 
-  Widget _monthPicker() {
+  Widget monthPicker() {
     return Row(
       children: [
         const Text(
@@ -87,7 +163,7 @@ class CalendarPage extends StatelessWidget {
     );
   }
 
-  Widget _todayButton() {
+  Widget todayButton() {
     return OutlinedButton(
       onPressed: () {},
       style: OutlinedButton.styleFrom(
